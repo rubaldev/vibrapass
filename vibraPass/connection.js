00efcfdf9d60053429e1777b104b4648ba52b6bb
rubaldev <balabalaruphin@gmail.com>
2025-02-06 12:12:02 +0100

    Add files via upload
    version une de mon application de gestion de mots de pass

---
+++ vibraPass/connection.js
@@ -0,0 +1,117 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'vibrachat',
+    connectTimeout: 10000,
+})
+connection.connect((err) => {
+    if (err) {
+        console.error('erreur de connexion: ' + err.stack);
+        return;
+    }
+    console.log('connecté en tant que ID' + connection.threadId);
+});
+
+
+const selectAll = () => {
+    return new Promise((resolve, reject) => {
+        connection.query('select *  from user', (err, results) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(results);
+        });
+    });
+};
+const insertIntoDb = (name, secondname, christname, email, password, sexe) => {
+    return new Promise((resolve, reject) => {
+        var request = `INSERT INTO user(name_user,second_name,christ_name,email,password,sexe) VALUES('${name}', '${secondname}', '${christname}', '${email}', '${password}','${sexe}')`;
+        connection.query(request, (err, results) => {
+            if (err) {
+                console.log("une erreur s'est produite", err);
+                reject(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+};
+const verifyUser = async (email, password) => {
+    var request = `SELECT * FROM user WHERE email = '${email}' AND password = '${password}'`;
+    return new Promise((resolve, reject) => {
+        connection.query(request, (err, results) => {
+            if (err) {
+                reject(results)
+                console.log(err);
+            }
+            else {
+                resolve(results);
+            }
+        })
+    })
+
+};
+/*
+const getIdentity = (email, password) => {
+    const request = `SELECT name_user,second_name WHERE email ='${email}' AND password = '${password}'`;
+    return new Promise((resolve, reject) => {
+        connection.query(request, (err, results) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}*/
+const deleteUser = (email, password) => {
+    const request = `DELETE FROM user WHERE email = '${email}' AND password = '${password}'`
+    return new Promise((resolve, reject) => {
+        connection.query(request, (err, results) => {
+            if (err) {
+                console.log(err);
+                reject(false)
+            }
+            else {
+                resolve(true)
+            }
+        })
+    })
+}
+const getPassWord = (id_user = 135) => {
+    const request = `SELECT * FROM template where id_user = '${id_user}'`;
+    return new Promise((resolve, reject) => {
+        connection.query(request, (err, results) => {
+            if (err) {
+                console.log(err)
+                reject(false)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}
+const insertUserTemplate = (email, website, category, passwordsite, id_user) => {
+    const request = `INSERT INTO template(email,website,category,passwordsite,id_user) VALUES('${email}','${website}','${category}','${passwordsite}','${id_user}')`
+    return new Promise((resolve, reject) => {
+        connection.query(request, (err, results) => {
+            if (err) {
+                reject(false)
+                console.log(err)
+            } else {
+                resolve(true)
+            }
+        })
+    })
+}
+module.exports = {
+    selectAll,
+    insertIntoDb,
+    verifyUser,
+    //getIdentity,
+    deleteUser,
+    getPassWord,
+    insertUserTemplate
+}
---
+++ vibraPass/public/scrypte/bd.sql
@@ -0,0 +1,31 @@
+DROP DATABASE vibrachat;
+create DATABASE vibrachat;
+
+use vibrachat;
+create table user(id_user int primary key auto_increment,
+name_user varchar(30) NOT NULL,
+second_name varchar(30) NOT NULL,
+christ_name varchar(30) NOT NULL,
+email varchar(30) NOT NULL UNIQUE,
+password varchar(10) NOT NULL,
+sexe char(1))engine=innoDB;
+describe user;
+
+create table template(id_template int primary key auto_increment,
+email varchar(30),
+website varchar(100) NOT NULL,
+category varchar(30),
+passwordsite varchar(30) NOT NULL,
+id_user int,
+foreign key(id_user) references user(id_user)
+)engine=innoDB;
+describe template;
+/*
+create table message(id_message int primary key auto_increment,
+id_user_send int,
+message varchar(500),
+id_user_recever int,
+foreign key(id_user_send) references user(id_user),
+foreign key(id_user_recever) references user(id_user))engine=innoDB;
+describe message;
+*/
